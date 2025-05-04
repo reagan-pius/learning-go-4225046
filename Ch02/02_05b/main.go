fa5bdd28d30ef0d6dce541f830ddb3878bf771a8
rgnpx <99097454+reagan-pius@users.noreply.github.com>
2025-05-04 22:21:15 +0000

    math library

--- Ch02/02_05b/main.go
+++ Ch02/02_05b/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
 func main() {
@@ -9,5 +10,14 @@ func main() {
 	f1, f2, f3 := 23.5, 65.1, 76.3
 	sum := f1 + f2 + f3
 	fmt.Println("Float sum:", sum)
+
+	sum = math.Round(sum*100) / 100
+	fmt.Println("The sum is now \n", sum)
+
+	fmt.Println(("the value of pi is"), math.Pi)
+
+	circleradius := 5.0
+	circumference := 2 * math.Pi * circleradius
+	fmt.Println("Circumference of circle with radius", circleradius, "is %.2f\n", circumference)
 
 }
