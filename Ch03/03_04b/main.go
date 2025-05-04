f626f30f9f7cc90bd693c74e8f923cef82fe94ab
rgnpx <99097454+reagan-pius@users.noreply.github.com>
2025-05-04 23:55:09 +0000

    slices

--- Ch03/03_04b/main.go
+++ Ch03/03_04b/main.go
@@ -2,10 +2,30 @@ package main
 
 import (
 	"fmt"
+	"sort"
 )
 
 func main() {
-	// This is an array
-	var colors = [3]string{"Red", "Green", "Blue"}
+	// This is a slice
+	//var colors = [3]string{"Red", "Green", "Blue"}
+
+	var colors = make([]string, 0, 3)
+	colors = append(colors, "Red", "Green", "Blue")
+	colors = append(colors, "Yellow", "Black", "White")
 	fmt.Println(colors)
+	fmt.Println("The length of colors:", len(colors))
+	// fmt.Println("The capacity of colors:", cap(colors))
+	// The length of the slice is the number of elements in it
+
+	colors = remove(colors, 0)
+	// fmt.Println(colors)
+
+	sort.Strings(colors)
+	fmt.Println(colors)
+	colors = remove(colors, 0)
+	fmt.Println(colors)
+}
+
+func remove(slice []string, i int) []string {
+	return append(slice[:i], slice[i+1:]...)
 }
