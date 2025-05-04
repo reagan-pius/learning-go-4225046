094bf7f64fa97b6bfa1a0b4b1f814450d0f6a63a
rgnpx <99097454+reagan-pius@users.noreply.github.com>
2025-05-04 22:36:57 +0000

    date and time

--- Ch02/02_06b/main.go
+++ Ch02/02_06b/main.go
@@ -2,10 +2,23 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
 func main() {
 
-	fmt.Println("Dates and times")
+	//fmt.Println("Dates and times")
+	t := time.Date(2009, time.November, 10, 23, 0, 0, 0, time.UTC)
+	fmt.Printf("Go was launched at %s\n", t)
 
+	n := time.Now()
+	fmt.Printf("Current time: %s\n", n)
+	fmt.Printf("The object's type: %T\n", n)
+	fmt.Printf("The object's value: %v\n", n)
+
+
+	fmt.Printf(n.Format((time.ANSIC)) + "\n")
+
+	tomorrow := n.AddDate(0, 0, 1)
+	fmt.Printf(tomorrow.Format(time.ANSIC) + "\n")
 }
