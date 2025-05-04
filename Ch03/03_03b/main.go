137eb75bb68967aed933c1b5e2dc19e8e9dbcc9b
rgnpx <99097454+reagan-pius@users.noreply.github.com>
2025-05-04 23:35:47 +0000

    arrays

--- Ch03/03_03b/main.go
+++ Ch03/03_03b/main.go
@@ -5,5 +5,14 @@ import (
 )
 
 func main() {
-	fmt.Println("Arrays")
+	var colors [3]string
+	colors[0] = "red"
+	colors[1] = "green"
+	colors[2] = "blue"
+	fmt.Println(colors)
+
+	var numbers = [5]int{5, 3, 2, 4, 1}
+	fmt.Println(numbers)
+	fmt.Println("The length of colors:", len(colors))
+	fmt.Println("The length of numbers:", len(numbers))
 }
