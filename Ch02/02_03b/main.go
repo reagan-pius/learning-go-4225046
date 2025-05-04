336de01145e702bbbc15d5f2a1f0d20216dbe536
rgnpx <99097454+reagan-pius@users.noreply.github.com>
2025-05-04 15:21:13 +0000

    get user input

--- Ch02/02_03b/main.go
+++ Ch02/02_03b/main.go
@@ -1,5 +1,28 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+	"strconv"
+)
+
 func main() {
 
+	// how to get a string from the user
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Enter text: ")
+	str, _ := reader.ReadString('\n')
+	fmt.Println(str)
+
+	// how to get a number from the user
+	fmt.Print("Enter a number: ")
+	str, _ = reader.ReadString('\n')
+	f, err := strconv.ParseFloat(strings.TrimSpace(str), 64)
+	if err != nil {
+		fmt.Println("Error:", err)
+	} else {
+		fmt.Println("Number:", f)
+	}
 }
