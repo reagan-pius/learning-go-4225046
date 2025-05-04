7a8c1918a1722f7f45c3e2c46b65aa10c76cb988
rgnpx <99097454+reagan-pius@users.noreply.github.com>
2025-05-04 15:08:48 +0000

    fmt

--- Ch02/02_02b/main.go
+++ Ch02/02_02b/main.go
@@ -6,9 +6,21 @@ import (
 
 func main() {
 
-	// str1 := "The quick red fox"
-	// str2 := "jumped over"
-	// str3 := "the lazy brown dog."
+	str1 := "The quick red fox"
+	str2 := "jumped over"
+	str3 := "the lazy brown dog."
+	aNumber := 42
 
-	fmt.Println("Hello from Go!")
+	fmt.Println(str1, str2, str3)
+	stringLength, err := fmt.Println("The value is", aNumber)
+	if err == nil {
+		fmt.Println("String length:", stringLength)
+	} else {
+		fmt.Println("Error:", err)
+	}
+
+	fmt.Printf("Value of number: %v\n", aNumber)
+
+	fmt.Printf("Datatype of number: %T\n", aNumber)
 }
+
