d7090b1066c8cbe63bc1c5e2587439cd18e0403e
rgnpx <99097454+reagan-pius@users.noreply.github.com>
2025-05-04 23:30:18 +0000

    Pointers

--- Ch03/03_02b/main.go
+++ Ch03/03_02b/main.go
@@ -5,5 +5,17 @@ import (
 )
 
 func main() {
-	fmt.Println("Pointers")
+	anInt := 42
+	var p *int = &anInt // p points to anInt
+	if p == nil {
+		fmt.Println("p is nil")
+	} else {
+		fmt.Println("The value of p:", *p)
+	}
+
+	value1 := 42.13
+	pointer1 := &value1 // ampersand operator (&) gives the address of value1
+	*pointer1 = *pointer1/31
+	fmt.Println(("Pointer: "), *pointer1)
+	fmt.Println("The original value:", value1)
 }
