984ff3d120ad2f6d39b81a1c2e1c1f4a905b95cb
rgnpx <99097454+reagan-pius@users.noreply.github.com>
2025-05-04 22:02:43 +0000

    arithmetic operations

--- Ch02/02_04b/main.go
+++ Ch02/02_04b/main.go
@@ -3,5 +3,20 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Math")
+	i1, i2, i3 := 1, 2, 3
+	intSum := i1 + i2 + i3
+	fmt.Println("Sum of integers:", intSum)
+	//fmt.Println("Math")
+
+
+	f1, f2, f3 := 1.1, 2.2, 3.3
+	floatSum := f1 + f2 + f3
+	fmt.Println("Sum of floats:", floatSum)
+
+	totalSum := float64(i1) + f2
+	fmt.Println("Sum of int and float:", totalSum)
+
+	product := float64(i1) * f2
+	fmt.Println("Product:", product)
+
 }
