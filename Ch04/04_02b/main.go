be842a93bfc3c46d23ca604ff890d27c087854a7
rgnpx <99097454+reagan-pius@users.noreply.github.com>
2025-05-05 13:12:19 +0000

    Switch cases

--- Ch04/04_02b/main.go
+++ Ch04/04_02b/main.go
@@ -9,8 +9,24 @@ func main() {
 
 	weekday := time.Now().Weekday()
 	fmt.Printf("Today is %v\n", weekday)
-
-	dayNumber := int(weekday)
+	dayNumber := 6
+	// dayNumber := int(weekday)
 	fmt.Printf("The day as a number is %v\n", dayNumber)
 
+	var result string
+	switch dayNumber {
+	case 1:
+		result = "Its a Monday!"
+	case 2:
+		result = "Its a Tuesday!"
+	case 3:
+		result = "Its a Wednesday!"
+	case 4:
+		result = "Its a Thursday!"
+	case 5:
+		result = "Its a Friday!"
+	default:
+		result = "Its a weekend!"	
+}
+fmt.Println(result)
 }
