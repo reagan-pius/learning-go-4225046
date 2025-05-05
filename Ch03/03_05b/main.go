58b875afab71218f036ed297af8fe7214936942a
rgnpx <99097454+reagan-pius@users.noreply.github.com>
2025-05-05 00:22:06 +0000

    Unordered slices - to check again

--- Ch03/03_05b/main.go
+++ Ch03/03_05b/main.go
@@ -2,8 +2,40 @@ package main
 
 import (
 	"fmt"
+	"sort"
 )
 
 func main() {
-	fmt.Println("Maps")
+	states := make(map[string]string)
+	states["Al"] = "Montgomery"
+	states["Alsk"] = "Juneau"
+	states["Ar"] = "Little Rock"
+	states["Ca"] = "California"
+	fmt.Println(states)
+
+
+	california := states["Ca"]
+	fmt.Println(california)
+	delete(states, "Ca")
+	fmt.Println(states)
+
+	states["NY"] = "NEW YORK"
+	fmt.Println(states)
+
+	for k, v := range states {
+		fmt.Printf("%v : %v\n", k, v)
+	}
+
+	keys := make([] string, len(states))
+	i := 0
+	for k := range states {
+		keys[i] = k
+		i++
+	}
+	sort.Strings(keys)
+	fmt.Println("\nSorted keys:")
+
+	for i := range keys {
+		fmt.Println(states[keys[i]])
+	}
 }
