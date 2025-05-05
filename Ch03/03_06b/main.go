bd72003473f132b601c2b48b870274d271481d46
rgnpx <99097454+reagan-pius@users.noreply.github.com>
2025-05-05 00:27:46 +0000

    structs 1

--- Ch03/03_06b/main.go
+++ Ch03/03_06b/main.go
@@ -5,5 +5,15 @@ import (
 )
 
 func main() {
-	fmt.Println("Structs")
+	poodle := Dog{"Poodle", 50}
+	fmt.Println(poodle)
+	fmt.Printf("%+v\n", poodle)
+	fmt.Printf("Breed: %v, Weight: %v\n", poodle.Breed, poodle.Weight)
+	poodle.Weight = 60
+	fmt.Println(poodle)
+}
+
+type Dog struct {
+	Breed string
+	Weight int
 }
