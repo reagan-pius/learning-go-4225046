295d8a5efa831542ff1f1c5c41281819242b77ba
rgnpx <99097454+reagan-pius@users.noreply.github.com>
2025-05-05 16:08:24 +0000

    Files

--- Ch06/06_01b/main.go
+++ Ch06/06_01b/main.go
@@ -2,8 +2,28 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"os"
 )
 
 func main() {
-	fmt.Println("Files")
+	fileName := "./fromString.txt"
+	file, err := os.Create(fileName)
+	defer file.Close()
+	checkErr(err)
+	length, err := io.WriteString(file, "Hello, World!")
+	fmt.Printf("Wrote a file with %v characters\n", length)
+}
+
+func checkErr(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func readFile(fileName string) {
+	file, err := os.ReadFile(fileName)
+	checkErr(err)
+	fmt.Println("Text read from file:", string(file))
+
 }
