52ee1187fd798e8f5ecffcee9df37e7c56d25557
rgnpx <99097454+reagan-pius@users.noreply.github.com>
2025-05-05 12:59:58 +0000

    Conditionals

--- Ch04/04_01b/main.go
+++ Ch04/04_01b/main.go
@@ -5,5 +5,15 @@ import (
 )
 
 func main() {
-	fmt.Println("Conditional logic")
+	// theAnswer := -1
+	var result string
+
+	if theAnswer := 42; theAnswer < 0 {
+		result = "less than zero"
+	} else if theAnswer == 0 {
+		result = "equal to zero"
+	} else {
+		result = "greater than zero"
+	}
+	fmt.Println(result)
 }
