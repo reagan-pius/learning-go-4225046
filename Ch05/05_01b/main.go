e66f355da53d8f411f4aef429c422a559a135e75
rgnpx <99097454+reagan-pius@users.noreply.github.com>
2025-05-05 14:49:38 +0000

    Functions

--- Ch05/05_01b/main.go
+++ Ch05/05_01b/main.go
@@ -6,4 +6,28 @@ import (
 
 func main() {
 	fmt.Println("Functions")
+	doSomething()
+
+	add(4, 5, 6)
+
+
+	addAllValues(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+}
+func doSomething() {
+	fmt.Println("Doing something")
+} 
+
+func add(value1 int, value2 int, value3 int) int {
+	sum := value1 + value2 + value3
+	fmt.Printf("Sum: %v\n", sum)
+	return sum
+}
+
+func addAllValues(values ...int) int{
+	sum := 0
+	for _, value := range values {
+		sum += value
+	}
+	fmt.Printf("Sum: %v\n", sum)
+	return sum
 }
