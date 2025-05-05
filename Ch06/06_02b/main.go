e296e3e949b354d74219dc058db0a1fca751c9f3
rgnpx <99097454+reagan-pius@users.noreply.github.com>
2025-05-05 16:31:37 +0000

    requests

--- Ch06/06_02b/main.go
+++ Ch06/06_02b/main.go
@@ -2,16 +2,34 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+	"io"
 )
 
 const url = "http://services.explorecalifornia.org/json/tours.php"
 
 func main() {
 	fmt.Println("Network requests")
+	client := http.Client{}
+	req, err := http.NewRequest("GET", url, nil)
+	checkError(err)
+
+	req.Header.Set("User-Agent", "") // the api required a user agent but does not require a specific one
+
+	resp, err := client.Do(req)
+	checkError(err)
+	defer resp.Body.Close()
+	fmt.Printf("Response type: %T\n", resp)
+
+	bytes, err := io.ReadAll(resp.Body)
+	checkError(err)
+	content := string(bytes)
+	fmt.Print(content)
+
 }
 
-// func checkError(err error) {
-// 	if err != nil {
-// 		panic(err)
-// 	}
-// }
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
