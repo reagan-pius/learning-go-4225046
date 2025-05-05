29deecf8b3d371fc728d0ac9fd006f8670351538
rgnpx <99097454+reagan-pius@users.noreply.github.com>
2025-05-05 15:06:18 +0000

    Anonymous function and delay

--- Ch05/05_03b/main.go
+++ Ch05/05_03b/main.go
@@ -2,8 +2,22 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
 func main() {
-	fmt.Println("Goroutines")
+	go say("Hello from goroutine")
+	fmt.Println("Hello from main")
+
+	go func(message string){
+		fmt.Println(message)
+	}("Hello from anonymous function")
+	time.Sleep(2 * time.Second)	
+	fmt.Println("All done")
+}
+
+func say(message string) {
+	time.Sleep(1 * time.Second)
+	// fmt.Println(message)
+	fmt.Println(message)
 }
